94b02fe5446833a89fe909ec88cc251394375983
bob <rherro123@yahoo.com>
2024-03-11 10:10:55 -0400

    Add some security when saving private key to disk

--- src/generate_key_pair.py
+++ src/generate_key_pair.py
@@ -1,10 +1,12 @@
 from jwcrypto import jwk
 
+PEM_PASSWORD = 'WEAK_PASSWORD_FOR_DATA_AT_REST'.encode()
+
 
 def generate_key_pair():
     key = jwk.JWK.generate(kty='OKP', crv='Ed25519')
     public_key = key.export_to_pem(private_key=False)
-    private_key = key.export_to_pem(private_key=True, password=None)
+    private_key = key.export_to_pem(private_key=True, password=PEM_PASSWORD)
 
     with open('private_key.pem', 'wb') as f:
         f.write(private_key)
